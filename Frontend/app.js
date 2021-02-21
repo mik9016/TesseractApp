c3fd25778feb751291e4bc580698c59c90060d49
mik9016 <mikolajgruszecki@o2.pl>
2021-02-21 11:10:04 +0100

    add removing file handling

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,179 +1,158 @@
 //select url
-const url = 'http://localhost:3000/upload-avatar';
+const url = "http://localhost:3000/upload-picture";
 
 // select file input
-const input = document.getElementById('avatar');
+const input = document.getElementById("picture");
 
 //select spinner
-const spinner = document.getElementById('spinner');
+const spinner = document.getElementById("spinner");
 
 //select copy button
 
-const copyBtn = document.getElementById('copyBtn');
+const copyBtn = document.getElementById("copyBtn");
 
 //select body
 
-const body = document.getElementById('bod');
-
+const body = document.getElementById("bod");
 
 // add event listener
-input.addEventListener('change', () => {
-    
-    uploadFile(input.files[0]);
+input.addEventListener("change", () => {
+  uploadFile(input.files[0]);
 });
 /////////////////////////////////////////////////////
 //FUNCTIONS
 
-function spinnerOn(){
-    spinner.style.display="block";
-    body.style.backgroundColor= "grey"
-    
+function spinnerOn() {
+  spinner.style.display = "block";
+  body.style.backgroundColor = "grey";
+}
 
-};
-
-function spinnerOff(){
-    spinner.style.display="none";
-    body.style.backgroundColor= "whitesmoke"
+function spinnerOff() {
+  spinner.style.display = "none";
+  body.style.backgroundColor = "whitesmoke";
 }
 
 function handleErrors(response) {
-    if (!response.ok) {
-        alert('no file Uploaded!');
-        throw Error(response.statusText);
-        
-    }
-    return response;
-};
+  if (!response.ok) {
+    alert("no file Uploaded!");
+    throw Error(response.statusText);
+  }
+  return response;
+}
 
 //prepare consts to abort fetch requests
 
 const controller = new AbortController();
 
-function aborter(contr){ 
-    contr.abort();
+function aborter(contr) {
+  contr.abort();
 }
-
 
 //file extension check
 
+function checkFileType(file) {
+  //Allowed extensions
+  const filetypes = /jpeg|png|jpg/;
+  const lowFile = file.name.toLowerCase();
+  //check ext
+  const extname = filetypes.test(lowFile);
 
-function checkFileType(file){
-        //Allowed extensions
-        const filetypes = /jpeg|png|jpg/;
-        const lowFile = file.name.toLowerCase();
-        //check ext
-        const extname = filetypes.test(lowFile);
-        
-        if(!extname){
-            alert('Error: Images Only!');
-            return aborter(controller);
-            
-        };
-        
-        
-    };
+  if (!extname) {
+    alert("Error: Images Only!");
+    return aborter(controller);
+  }
+}
 //////////////////////////////////////////////////////////
 //fetching
 
-
 const uploadFile = (file) => {
+  // add file to FormData object
+  const fd = new FormData();
 
-    // add file to FormData object
-    const fd = new FormData();
+  //check Filetype
+  checkFileType(file);
 
-   //check Filetype
-    checkFileType(file);
+  fd.append("picture", file); //mus be set exactly like on the server line 37
 
-    fd.append('avatar', file);//musi byc tak ustawiony jak w serverze line 37
+  //cancel post request if file has wrong extension
 
-    //cancel post request if file has wrong extension 
-    
-    const signal = controller.signal;
-    // send `POST` request
-    fetch(url, {
-        method: 'POST',
-        signal: signal,
-        body: fd
-    })
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error(err));
+  const signal = controller.signal;
+  // send `POST` request
+  fetch(url, {
+    method: "POST",
+    signal: signal,
+    body: fd,
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error(err));
 };
 
 //GET
 
 //create a buttton
-const getBtn = document.getElementById('getBtn');
+const getBtn = document.getElementById("getBtn");
 
 //add image
 
-const img = document.getElementById('img');
+const img = document.getElementById("img");
 
-getBtn.addEventListener('click',()=>{
-    
+getBtn.addEventListener("click", () => {
+  fetch(url)
+    .then((res) => res.blob())
+    .then((data) => {
+      const rightType = "image/png";
+      //check if file is selected
+      if (data.type !== rightType) {
+        console.log(data.type);
+        alert("No File Selected or wrong file extension!");
+        location.reload();
+      } else {
+        console.log(data.type);
+        const objectURL = URL.createObjectURL(data);
 
+        img.src = objectURL;
 
-    fetch(url)
-    .then(res => res.blob())
-    .then(data => {
-        const rightType = 'image/png';
-            //check if file is selected
-            if (data.type !== rightType) {
-                console.log(data.type)
-                alert('No File Selected or wrong file extension!');
-                location.reload();
-            }else{
+        //tesseract
+        const exampleImage = img.src; //image path
 
-            console.log(data.type)
-            const objectURL = URL.createObjectURL(data);
-        
-            img.src = objectURL;
+        const worker = Tesseract.createWorker({
+          logger: (m) => console.log(m),
+          spinner: spinnerOn(),
+        });
+        Tesseract.setLogging(true);
+        work();
 
-            //tesseract 
-            const exampleImage = img.src;//image path 
+        async function work() {
+          await worker.load();
+          await worker.loadLanguage("eng", "deu");
+          await worker.initialize("eng", "deu");
 
-            const worker = Tesseract.createWorker({
-            logger: m => console.log(m),
-            spinner: spinnerOn()
-            }
-           );
-            Tesseract.setLogging(true);
-            work();
+          let result = await worker.detect(exampleImage);
 
-            async function work() {
-            await worker.load();
-            await worker.loadLanguage('eng','deu');
-            await worker.initialize('eng','deu');
+          result = await worker.recognize(exampleImage);
 
-            let result = await worker.detect(exampleImage);
-            
+          const output = document.getElementById("tes");
+          let resultText = result.data.text;
+          output.append(resultText);
+          spinnerOff();
 
-            result = await worker.recognize(exampleImage);
+          copyBtn.addEventListener("click", () => {
+            output.select();
+            document.execCommand("copy");
+            alert("Text copied");
+          });
 
-            const output = document.getElementById('tes');
-            let resultText = result.data.text;
-            output.append(resultText);
-            spinnerOff();
-            
-            copyBtn.addEventListener('click',()=>{
-                output.select();
-                document.execCommand("copy");
-                alert('Text copied');
-            })
-                
-            await worker.terminate();
+          await worker.terminate();
         }
-            }
-       
+      }
     })
-    .catch(err=>{console.log(err)});
-    
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-const resetBtn = document.getElementById('reset');
-resetBtn.addEventListener('click',()=>{
-    location.reload();
-})
-
-
-
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", () => {
+  location.reload();
+});
--- index.js
+++ index.js
@@ -1,86 +1,76 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-// const morgan = require('morgan');
-// const _ = require('lodash');
-const path = require('path');
-
-
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
-
 // enable files upload
-app.use(fileUpload({
-    createParentPath: true
-}));
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
 //add other middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-// app.use(morgan('dev'));
-app.use(express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
 
-//start app 
+//start app
 const port = process.env.PORT || 3000;
-
-
 
 let imgArr = {};
 
-app.post('/upload-avatar', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file !!!
-            let avatar = req.files.avatar;
-            
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./uploads/' + avatar.name);
-            
-            //add filename to imgArr object
-            Object.assign(imgArr,avatar);
-            // console.log(imgArr.name);
+app.post("/upload-picture", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+      });
+    } else {
+      //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
+      let picture = req.files.picture;
 
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      picture.mv("./uploads/" + picture.name);
+
+      //add filename to imgArr object
+      Object.assign(imgArr, picture);
+      // console.log(imgArr.name);
+
+      //send response
+      res.send({
+        status: true,
+        message: "File is uploaded",
+        data: {
+          name: picture.name,
+          mimetype: picture.mimetype,
+          size: picture.size,
+        },
+      });
     }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get('/upload-avatar', function(req, res, next) {
-    try{
-        res.sendFile(path.join(__dirname + '/uploads/' + imgArr.name));//path to last file
-        fileUpload.FileArray
-    }catch(err){
-        res.send(err);
-    }
-    
-    
+app.get("/upload-picture", function (req, res, next) {
+  try {
+    res.sendFile(path.join(__dirname + "/uploads/" + imgArr.name)); //path to last file
+    fileUpload.FileArray;
+    setTimeout(() => {
+      fs.unlink(path.join(__dirname + "/uploads/" + imgArr.name), () => {
+        console.log("file removed");
+      });
+    },4000);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-  });
-
-
-
-
-app.listen(port, () => 
-  console.log(`App is listening on port ${port}.`)
-);
-
-
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
